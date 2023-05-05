995fc226d4c65853160d17aa4d3e54d157163bf3
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-06 01:17:08 +0200

    Alignment.CenterVertically

--- app/src/main/java/com/example/composedemo1/MainActivity.kt
+++ app/src/main/java/com/example/composedemo1/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : ComponentActivity() {
             Row(
                 modifier = Modifier.background(color = Color.LightGray )
                     .fillMaxSize(),
-                horizontalArrangement = Arrangement.Center
+                verticalAlignment = Alignment.CenterVertically
             ){
                 Greeting("AB")
                 Greeting("CDEF")
