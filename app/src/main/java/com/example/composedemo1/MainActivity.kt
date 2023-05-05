afdb41b6ff62e37210aaa92811dab95e6fcafb2d
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-06 00:49:16 +0200

    modifier simple example

--- app/src/main/java/com/example/composedemo1/MainActivity.kt
+++ app/src/main/java/com/example/composedemo1/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.composedemo1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composedemo1.ui.theme.ComposeDemo1Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,23 +27,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
-           // Greeting("Android")
-            
+            Greeting("Android")
+
 
         }
     }
 
 }
 
-/*@Composable
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!",
+    fontSize = 32.sp,
+    fontWeight = FontWeight.Bold,
+        color = Color.Red,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .background(color = Color.Yellow)
+            .border(2.dp, color = Color.Green)
+            .padding(10.dp)
+            .fillMaxWidth(0.5f)
+            .fillMaxHeight(0.3f)
+
+    )
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeDemo1Theme {
-        Greeting("KMP")
+        Greeting("Android")
     }
-}*/
+}
